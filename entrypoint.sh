6697e8e9f718f11c491cc247f1f8ac23c6dd51bc
Scott Currie <vitriolik@gmail.com>
2019-02-04 17:10:53 -0800

    Reverted changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,10 @@ cd /src
 
 # coverage run manage.py test -v 2 && \
 # coverage report
-python3 ./peAk/manage.py makemigrations --noinput
-python3 ./peAk/manage.py migrate --noinput
-python3 ./peAk/manage.py collectstatic --noinput && \
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput && \
 # python3 load_db.py
-python3 ./peAk/manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
 
 # gunicorn kickstarter_project.wsgi:application -w 3 -b :8000
