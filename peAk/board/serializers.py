dff948f49c901b14519a088c60ceafb91ca75ec6
Scott Currie <vitriolik@gmail.com>
2019-02-05 14:25:06 -0800

    Added fields to Resort serializer

--- peAk/board/serializers.py
+++ peAk/board/serializers.py
@@ -8,7 +8,9 @@ class ResortSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resort
         #TODO: Add additional Resort fields as needed
-        fields = ('id', 'name')
+        fields = ('id', 'resort_name', 'resort_location_lat', 'resort_location_long', 'resort_address_line1',
+                  'resort_address_line2', 'resort_address_city', 'resort_address_state', 'resort_address_zip_code',
+                  'resort_website_url', 'resort_altitude')
 
 class UserSerializer(serializers.ModelSerializer):
     #TODO: build UserSerializer class
