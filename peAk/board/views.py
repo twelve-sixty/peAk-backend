1ec7b6fcc030c461d2124d13a3290324b88236e5
Scott Currie <vitriolik@gmail.com>
2019-02-04 16:54:09 -0800

    Stubbed out views. Resort list view in progress.

--- peAk/board/views.py
+++ peAk/board/views.py
@@ -1,32 +1,56 @@
-from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .serializers import ResortSerializer, UserSerializer, TeamSerializer, MessageSerializer
+from .models import Resort
 
+#TODO: Uncomment auth lines when app auth is working
 
 class ResortListApiView(generics.ListAPIView):
-    pass
+    """List or create Budget objects in API."""
+    # permission_classes = (IsAuthenticated,)
+    # authentication_classes = (TokenAuthentication, )
+    serializer_class = ResortSerializer
+
+    #https://stackoverflow.com/questions/3711349/django-and-query-string-parameters
+
+    def get_queryset(self):
+        lat = self.request.GET.get('latitude')
+        long = self.request.GET.get('longitude')
+        radius = self.request.GET.get('radius')
+        if lat and long:
+            if radius:
+                # TODO: Figure out how to filter Resort objects by distance from phone
+                pass
+
+        #TODO: Change .all() to .filter when the filter is figured out
+        return Resort.objects.all()
 
 
 class ResortDetailApiView(generics.RetrieveAPIView):
-    pass
+    serializer_class = ResortSerializer
 
+    def get_queryset(self):
+        return Resort.objects.filter(id=self.kwargs['pk'])
+
+#TODO: Build out view
 
 class UserDetailApiView(generics.RetrieveAPIView):
     pass
 
+#TODO: Build out view
 
 class TeamListView(generics.ListCreateAPIView):
     pass
 
-
+#TODO: Build out view
 class TeamDetailView(generics.RetrieveAPIView):
     pass
 
-
+#TODO: Build out view
 class MessageListView(generics.ListAPIView):
     pass
 
-
+#TODO: Build out view
 class MessageDetailView(generics.RetrieveAPIView):
     pass
