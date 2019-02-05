9bf4ef5cab30fa70b5099b0970b1320a5bfba5fe
tobyatgithub <toby.fangyuan@gmail.com>
2019-02-05 10:00:39 -0800

    chat debugging redis.

--- peAk/chat/consumers.py
+++ peAk/chat/consumers.py
@@ -1,66 +1,66 @@
-from channels.generic.websocket import WebsocketConsumer
-import json
-
-
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
-
-    def disconnect(self, close_code):
-        pass
-
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
-
-
-# from asgiref.sync import async_to_sync
 # from channels.generic.websocket import WebsocketConsumer
 # import json
 
 
 # class ChatConsumer(WebsocketConsumer):
 #     def connect(self):
-#         # obtain from URL route in chat/routing.py
-#         self.room_name = self.scope['url_route']['kwargs']['room_name']
-
-#         # create a channels group based on user-specified room name
-#         self.room_group_name = 'chat_%s' % self.room_name
-
-#         # join room group
-#         async_to_sync(self.channel_layer.group_add)(
-#             self.room_group_name,
-#             self.channel_name
-#         )
-
-#         # accepts websocket connection.
 #         self.accept()
 
 #     def disconnect(self, close_code):
-#         async_to_sync(self.channel_layer.group_discard)(
-#             self.room_group_name,
-#             self.channel_name
-#         )
+#         pass
 
 #     def receive(self, text_data):
 #         text_data_json = json.loads(text_data)
 #         message = text_data_json['message']
 
-#         async_to_sync(self.channel_layer.group_send)(
-#             self.room_group_name,
-#             {
-#                 'type': 'chat_message',
-#                 'message': message,
-#             }
-#         )
-
-#     def chat_message(self, event):
-#         message = event['message']
-
 #         self.send(text_data=json.dumps({
 #             'message': message
 #         }))
+
+
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+import json
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        # obtain from URL route in chat/routing.py
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+
+        # create a channels group based on user-specified room name
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        # accepts websocket connection.
+        self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+            }
+        )
+
+    def chat_message(self, event):
+        message = event['message']
+
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
--- peAk/peAk/settings.py
+++ peAk/peAk/settings.py
@@ -145,17 +145,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 REDIS_URL = 'redis://{}:6379'.format(os.getenv('REDIS_URL'))
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': REDIS_URL,
-        # 'LOCATION': 'redis://' + os.getenv('REDIS_URL'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
-        },
-        'KEY_PREFIX': 'example'
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django_redis.cache.RedisCache',
+#         'LOCATION': REDIS_URL,
+#         # 'LOCATION': 'redis://' + os.getenv('REDIS_URL'),
+#         'OPTIONS': {
+#             'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+#         },
+#         'KEY_PREFIX': 'example'
+#     }
+# }
 
 # cache timeout
 CACHE_TTL = 60 * 1
@@ -166,12 +166,13 @@ ASGI_APPLICATION = 'peAk.routing.application'
 
 # redis_host = os.getenv('REDIS_HOST', 'localhost')
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         # This example app uses the Redis channel layer implementation channels_redis
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [(redis_host, 6379)],
-#         },
-#     },
-# }
+CHANNEL_LAYERS = {
+    "default": {
+        # This example app uses the Redis channel layer implementation channels_redis
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [(REDIS_URL, 'localhost', 6379)],
+            # "hosts": [(redis_host, 6379)],
+        },
+    },
+}
