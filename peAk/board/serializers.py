8b13f78bf931c29a8da51c7c878cecf92cc17bd5
Scott Currie <vitriolik@gmail.com>
2019-02-05 14:46:20 -0800

    Added serializers

--- peAk/board/serializers.py
+++ peAk/board/serializers.py
@@ -7,15 +7,35 @@ class ResortSerializer(serializers.ModelSerializer):
     """Create serialized Resort objects to serve from the API."""
     class Meta:
         model = Resort
-        #TODO: Add additional Resort fields as needed
         fields = ('id', 'resort_name', 'resort_location_lat', 'resort_location_long', 'resort_address_line1',
                   'resort_address_line2', 'resort_address_city', 'resort_address_state', 'resort_address_zip_code',
                   'resort_website_url', 'resort_altitude')
 
 class UserSerializer(serializers.ModelSerializer):
     #TODO: build UserSerializer class
-    pass
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'user_email',
+            'user_name_first',
+            'user_name_last',
+            'user_fav_resort',
+            'user_date_of_birth',
+            'user_groups_belong',
+            'user_profile_picture',
+            'user_date_joined')
 
+    def create(self, validated_data):
+        user = super().create({
+            'username': validated_data['username'],
+            'email': validated_data['email']
+        })
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 class TeamSerializer(serializers.ModelSerializer):
     #TODO: build TeamSerializer class
