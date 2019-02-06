8d32871ffac5f42dfc27c24498ee769a00f05212
Scott Currie <vitriolik@gmail.com>
2019-02-06 13:55:42 -0800

    Reordered serializers. Added TeamSerializer as field in ResortSerializer.

--- peAk/board/serializers.py
+++ peAk/board/serializers.py
@@ -3,6 +3,27 @@ from rest_framework import serializers
 from .models import Resort, PeakUser, Team
 
 
+class TeamSerializer(serializers.ModelSerializer):
+    name = serializers.ReadOnlyField(source='team_name')
+    description = serializers.ReadOnlyField(source='team_description')
+    # TODO: Add a currentCapacity that updates whenever a user is added to a
+    # team
+    currentCapacity = serializers.ReadOnlyField(source='team_max_capacity')
+    maxCapacity = serializers.ReadOnlyField(source='team_max_capacity')
+    meetDate = serializers.ReadOnlyField(source='team_meet_date')
+    status = serializers.ReadOnlyField(source='team_status')
+
+    class Meta:
+        model = Team
+        fields = (
+            'id',
+            'name',
+            'description',
+            'currentCapacity',
+            'maxCapacity',
+            'meetDate',
+            'status')
+
 class ResortSerializer(serializers.ModelSerializer):
     """Create serialized Resort objects to serve from the API."""
     name = serializers.ReadOnlyField(source='resort_name')
@@ -10,58 +31,51 @@ class ResortSerializer(serializers.ModelSerializer):
     longitude = serializers.ReadOnlyField(source='resort_location_longitude')
     websiteUrl = serializers.ReadOnlyField(source='resort_website_url')
     altitude = serializers.ReadOnlyField(source='resort_altitude')
-    #TODO: Map address fields into address: {..} per spec provided by JAVA team
+    # Nested serializer for including teams in resort JSON
+    teams = TeamSerializer(many=True)
+    # TODO: Map address fields into address: {..} per spec provided by JAVA team
+
     class Meta:
         model = Resort
 
-        fields = ('id', 'name', 'latitude', 'longitude', 'resort_address_line1',
-                  'resort_address_line2', 'resort_address_city', 'resort_address_state', 'resort_address_zip_code',
-                  'websiteUrl', 'altitude')
+        fields = (
+            'id',
+            'name',
+            'latitude',
+            'longitude',
+            'resort_address_line1',
+            'resort_address_line2',
+            'resort_address_city',
+            'resort_address_state',
+            'resort_address_zip_code',
+            'websiteUrl',
+            'altitude',
+            'teams'
+        )
+
 
 class UserSerializer(serializers.ModelSerializer):
-    #TODO: build UserSerializer class
-    password = serializers.CharField(write_only=True)
-    email = serializers.Field(source='user_email')
+    # TODO: build UserSerializer class
+    username = serializers.ReadOnlyField(source='user_username')
+    bio = serializers.ReadOnlyField(source='user_bio')
+    # TODO: turn age into an actual calculation from birthdate
+    age = serializers.ReadOnlyField(source='user_date_of_birth')
+    favResort = serializers.ReadOnlyField(source='user_fav_resort')
 
     class Meta:
         model = PeakUser
         fields = (
             'id',
             'username',
-            'email',
-            'user_name_first',
-            'user_name_last',
-            'user_fav_resort',
-            'user_date_of_birth',
-            'user_groups_belong',
-            'user_profile_picture',
-            'user_date_joined')
+            'bio',
+            'age',
+            'favResort'
+        )
 
-    def create(self, validated_data):
-        user = super().create({
-            'username': validated_data['username'],
-            'email': validated_data['email']
-        })
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
 
-class TeamSerializer(serializers.ModelSerializer):
-    name = serializers.ReadOnlyField(source='team_name')
-    description = serializers.ReadOnlyField(source='team_description')
-    #TODO: Add a currentCapacity that updates whenever a user is added to a team
-    currentCapacity = serializers.ReadOnlyField(source='team_max_capacity')
-    maxCapacity = serializers.ReadOnlyField(source='team_max_capacity')
-    meetDate = serializers.ReadOnlyField(source='team_meet_date')
-    #TODO: resort is not serializable. How do we represent that?
-    # resort = serializers.ReadOnlyField(source='team_resort')
-    status = serializers.ReadOnlyField(source='team_status')
 
-    class Meta:
-        model = Team
-        fields = ('id', 'name', 'description', 'currentCapacity', 'maxCapacity', 'meetDate', 'status')
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    #TODO: build MessageSerializer class
+    # TODO: build MessageSerializer class
     pass
