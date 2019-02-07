fd619231fe057b40e092967fe7f4523856721cee
Scott Currie <vitriolik@gmail.com>
2019-02-07 14:14:44 -0800

    Removed delete from TeamDetailSerializer. Replaced with TeamCreateSerializer.

--- peAk/board/serializers.py
+++ peAk/board/serializers.py
@@ -49,6 +49,7 @@ class TeamOverviewSerializer(serializers.ModelSerializer):
             'meetDate',
             'status')
 
+
 class TeamDetailSerializer(serializers.ModelSerializer):
     """Serializes Team objects for detailed view. This includes serialized User objects
     representing members of the Team."""
@@ -61,7 +62,7 @@ class TeamDetailSerializer(serializers.ModelSerializer):
     maxCapacity = serializers.ReadOnlyField(source='team_max_capacity')
     meetDate = serializers.ReadOnlyField(source='team_meet_date')
     status = serializers.ReadOnlyField(source='team_status')
-    users = UserSerializer(many=True)
+    users = UserSerializer(many=True, default=None)
 
     class Meta:
         model = Team
@@ -74,6 +75,41 @@ class TeamDetailSerializer(serializers.ModelSerializer):
             'meetDate',
             'status',
             'users')
+
+
+class TeamCreateSerializer(serializers.ModelSerializer):
+    """Serializes Team objects for creation."""
+
+    # TODO: Add a currentCapacity that updates whenever a user is added to a team
+
+    class Meta:
+        model = Team
+        fields = (
+            'id',
+            'team_name',
+            'team_description',
+            'team_max_capacity',
+            'team_meet_date',
+            'team_status',
+            'team_tags',
+            'team_administrator',
+            'team_resort'
+            # 'users'
+        )
+
+    def create(self, validated_data):
+        # print('**validated_data**', validated_data)
+        team = super().create({
+            'team_name': validated_data['team_name'],
+            'team_meet_date': validated_data['team_meet_date'],
+            'team_max_capacity': validated_data['team_max_capacity'],
+            'team_description': validated_data['team_description'],
+            'team_administrator': validated_data['team_administrator'],
+            'team_resort': validated_data['team_resort']
+        })
+        team.save()
+        return team
+
 
 class ResortListSerializer(serializers.ModelSerializer):
     """Create serialized Resort objects to serve from the API. This returns the minimal detail
